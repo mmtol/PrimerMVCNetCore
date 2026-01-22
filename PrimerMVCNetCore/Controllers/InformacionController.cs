416bde7561baa3201122d996f48b07f9f74f915e
mmtol <mmigueltolosana@gmail.com>
2026-01-22 09:45:02 +0100

    Add project files.

---
+++ PrimerMVCNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            //vamos a enviar informacion simple a nuestra vista
+            ViewData["Nombre"] = "Alumno";
+            ViewData["Edad"] = "20";
+            ViewBag.DiaSemana = "Jueves";
+            return View();
+        }
+    }
+}
